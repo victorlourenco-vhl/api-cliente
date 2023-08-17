5fee26a525e04a351e2b7aafb02f2c22da3df08f
Victor Lourenço <victorhlo12@hotmail.com>
2023-08-17 12:06:08 -0300

    FEAT: Adicionando o ServiceRepository

---
+++ src/main/java/com/victor/apiclientes/repositories/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.victor.apiclientes.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victor.apiclientes.entities.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer>{
+
+}
