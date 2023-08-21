67049bf7da623f5667e5b4b53295f3fa5ddcc4d7
Victor Lourenço <victorhlo12@hotmail.com>
2023-08-21 10:26:29 -0300

    FEAT: Configurando a internacionalização das mensagens de exceção

--- src/main/java/com/victor/apiclientes/config/InternacionalizacaoConfig.java
+++ src/main/java/com/victor/apiclientes/config/InternacionalizacaoConfig.java
@@ -1,5 +1,33 @@
 package com.victor.apiclientes.config;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
 public class InternacionalizacaoConfig {
+	
+	@Bean
+	public MessageSource messageSource() {
+		// O ResourceBundle deixa subentendido que é um arquivo .properties
+		ReloadableResourceBundleMessageSource msgSource = new ReloadableResourceBundleMessageSource();
+		msgSource.setBasename("messages");
+		msgSource.setDefaultEncoding("ISO-8859-1");
+		msgSource.setDefaultLocale(Locale.getDefault());
+		
+		return msgSource;
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validatorFactoryBean() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+				
+	}
 
 }
